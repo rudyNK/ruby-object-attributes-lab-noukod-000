ba252613c556ecd182538aef76412755e0121aca
Rudy Ravelin <rudy@noukod.com>
2018-09-05 14:24:56 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,34 @@
+def Person
+
+  def name=(person_name)
+    @namev= person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+Beyonce = Person.new
+Beyonce.name = "Beyonce"
+
+    
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
